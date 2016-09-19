ed4e267c47acf7db4e3394c6ae83de0636c65601
Satta <sattaravi@gmail.com>
2016-09-19 12:59:14 -0700

    Update job.groovy

--- result/job.groovy
+++ result/job.groovy
@@ -1,6 +1,6 @@
 node{
     
-    docker.withRegistry('dtrnodednsq7g5ypoz2cbpo.westus.cloudapp.azure.com', 'ddc-dtr-login') {
+    docker.withRegistry('https://dtrnodednsq7g5ypoz2cbpo.westus.cloudapp.azure.com', 'ddc-dtr-login') {
         git 'https://github.com/SattaRavi/example-voting-app.git'
 
         docker.build('admin/com.sysgain.voting-app.result','./result').push('latest')
